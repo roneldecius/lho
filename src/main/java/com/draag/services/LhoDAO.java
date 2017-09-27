319faa7a122928a844b5f91c3b2ac0b764d569d6
deciusr <deciusr@10.139.11.121>
2017-09-27 17:03:54 -0400

    Adding property for db connection
    This will allow the system to use a different configuration in
    development and live.

--- src/main/java/com/draag/services/LhoDAO.java
+++ src/main/java/com/draag/services/LhoDAO.java
@@ -2,6 +2,8 @@ package com.draag.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
@@ -16,13 +18,46 @@ public class LhoDAO {
 	
 	private static LhoDAO dao;
 	
+	private enum CredentialEnum{
+		URL("mongo.db.url"),
+		DBNAME("mongo.db.name"),
+		USER("mongo.db.user"),
+		PASSWORD("mongo.db.password");
+		
+		private String key;
+		
+		CredentialEnum(String key){
+			this.key = key;
+		}
+		
+		public String value(ResourceBundle rs){
+			return rs.getString(key);
+		}
+			
+	};
+	
 	private LhoDAO(){
 		morphia = new Morphia().mapPackage("com.draag.services.model");
-		ServerAddress serverAddress = new ServerAddress();
-		MongoCredential credential = MongoCredential.createCredential("lhouser", "lhodb", "K0langy3t".toCharArray());
+		ServerAddress serverAddress = null;
+		
+		/* this is to facilitate the deployment of code without changing development
+		 * 
+		 * ONLY the live property file should be push upstream
+		 * 
+		 */
+		ResourceBundle bundle;
+		try {
+			bundle = ResourceBundle.getBundle("connectiondev");
+		} catch(MissingResourceException e){
+			bundle = ResourceBundle.getBundle("connectionlive");
+		}
+		
+		serverAddress = new ServerAddress(CredentialEnum.URL.value(bundle));
+		
+		MongoCredential credential = MongoCredential.createCredential(CredentialEnum.USER.value(bundle), CredentialEnum.DBNAME.value(bundle), CredentialEnum.PASSWORD.value(bundle).toCharArray());
 		List<MongoCredential> credentials = new ArrayList<>();
 		credentials.add(credential);
-		this.datastore = morphia.createDatastore(new MongoClient(serverAddress, credentials), "lhodb");
+		this.datastore = morphia.createDatastore(new MongoClient(serverAddress, credentials), CredentialEnum.DBNAME.value(bundle));
 		
 	}
 	
